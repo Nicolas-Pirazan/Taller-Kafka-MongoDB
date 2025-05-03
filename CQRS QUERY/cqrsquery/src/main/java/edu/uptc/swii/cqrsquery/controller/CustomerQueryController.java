ea475e54a63f24301d393e45fd6df80b50411064
Nicolas-Pirazan <143041203+Nicolas-Pirazan@users.noreply.github.com>
2025-05-03 14:45:12 -0500

    Add files via upload

---
+++ CQRS QUERY/cqrsquery/src/main/java/edu/uptc/swii/cqrsquery/controller/CustomerQueryController.java
@@ -0,0 +1,34 @@
+package edu.uptc.swii.cqrsquery.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import edu.uptc.swii.cqrsquery.model.Customer;
+import edu.uptc.swii.cqrsquery.repository.CustomerRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class CustomerQueryController {
+    private final CustomerRepository customerRepository;
+
+    public CustomerQueryController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/allcustomers")
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/findcustomerbyid")
+    public Customer getCustomerById(@RequestParam String id) {
+        return customerRepository.findById(id).get();
+    }
+    
+    @PostMapping("/add")
+    public Customer getCustomerById(@RequestParam Customer id) {
+        return customerRepository.save(id);
+    }
+    
+}
